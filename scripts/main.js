2a478a64efe66bafa35a9f5295f359a6cc381c5f
Hanson Tram <htram.dev@gmail.com>
2023-10-11 11:22:09 -0700

    Complete login page

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const nav = document.querySelectorAll('.nav');
 
-
-console.log(nav)
-
 navToggle.addEventListener('click', () => {
     nav[0].classList.toggle('nav--visible');
     nav[1].classList.toggle('nav--visible');
@@ -13,14 +10,14 @@ navToggle.addEventListener('click', () => {
 
 //DOCS: https://developer.mozilla.org/en-US/docs/Web/API/Window/resize_event
 
-const widthOutput = document.getElementsByTagName("body")[0]; // find the width on this element
+// const widthOutput = document.getElementsByTagName("body")[0]; // find the width on this element
 
 
-addEventListener("resize", (event) => {
-    const bodyWidth = widthOutput.offsetWidth;
-    console.log("Body width:", bodyWidth)
-    if (bodyWidth >= 900){
-        nav[0].classList.remove('toggle-row');
-        nav[1].classList.remove('nav--visible');
-    }
-});
+// addEventListener("resize", (event) => {
+//     const bodyWidth = widthOutput.offsetWidth;
+//     console.log("Body width:", bodyWidth)
+//     if (bodyWidth >= 900){
+//         // nav[0].classList.remove('toggle-row');
+//         // nav[1].classList.remove('nav--visible');
+//     }
+// });
